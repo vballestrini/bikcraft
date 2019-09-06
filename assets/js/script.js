57930eeb4396378e862202dc58cd90e734db8a4a
Víctor Ballestrini <vsballestrini@gmail.com>
2019-09-06 14:46:23 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"
+        nav: false, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000 // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: "portifolio", // nome do atributo data-slide="principal"
+        nav: true, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000 // tempo de transição dos slides
+    });
+}
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
